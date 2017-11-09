016f929ae8f086eeddd7540c61f1005f6c3f3330
ShotPurple <shotpurple@hotmail.com>
2017-11-09 21:38:16 +0000

    Added javascript files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Page has loaded! I can't believe it's not butter!");
+function helloWorld() {
+  console.log('Hello 2nd World!');
+}
+ function buttonThree() {
+   document.getElementById('headerthree').innerHTML = 'Hello Developing World!';
+   console.log('button three pressed - release the cows');
+ }
